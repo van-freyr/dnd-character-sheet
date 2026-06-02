6805c9ac31d25b886ecab5df952553fcc73094ff
van-freyr <ichisephy@gmail.com>
2026-06-02 04:16:40 -0400

    Add basic App component

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>D&D Character Sheet</h1>
+      <p>Welcome! Your character sheet is loading...</p>
+    </div>
+  );
+}
